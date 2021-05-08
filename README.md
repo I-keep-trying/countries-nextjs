360bb627b9cf280a0c68d1bedf44832f37f9555d
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2021-05-08 11:41:16 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+cloned from: https://github.com/nghiemthu/world-ranks
+
+good tutorial/article: https://www.learnbestcoding.com/post/19/ssr-react-app-with-next-js
---
+++ src/pages/index.js
@@ -0,0 +1,61 @@
+import Head from 'next/head'
+import { useState } from 'react'
+import CountriesTable from '../components/CountriesTable/CountriesTable'
+import Layout from '../components/Layout/Layout'
+import SearchInput from '../components/SearchInput/SearchInput'
+import styles from '../styles/Home.module.css'
+
+export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState('')
+
+  /*   const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  ); */
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().startsWith(keyword)
+  )
+
+  const onInputChange = (e) => {
+    e.preventDefault()
+    setKeyword(e.target.value.toLowerCase())
+  }
+
+  return (
+    <Layout>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>
+        {filteredCountries.length === 1 ? 
+        (<>Found {filteredCountries.length} country </>)
+        :
+        (<>Found {filteredCountries.length} countries</>)
+        }
+        </div>
+
+        <div className={styles.input}>
+          <SearchInput
+            placeholder="Start typing country name..."
+            onChange={onInputChange}
+          />
+        </div>
+      </div>
+
+      <CountriesTable countries={filteredCountries} />
+    </Layout>
+  )
+}
+
+export const getStaticProps = async () => {
+  // console.log('src/pages/index.js process.env.NEXT_PUBLIC_HERE_KEY',process.env.NEXT_PUBLIC_HERE_KEY)
+  const res = await fetch('https://restcountries.eu/rest/v2/all')
+  const countries = await res.json()
+
+  return {
+    props: {
+      countries,
+    },
+  }
+}
